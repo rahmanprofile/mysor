07080923e96903ea89d8bfadacb7e47ca580e2f5
Aimbor <aimbor.info@gmail.com>
2024-12-05 22:10:57 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/hyler/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hyler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:hyler/utilities/face_detector_view.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Home(),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Face Detection',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const FaceDetectorView()));
+            },
+            child: const Text("Let's View"),
+        ),
+      ),
+    );
+  }
+}
